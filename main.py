b05fd09133b43067761dccef03ea4e0262729b42
Michael Flora <michael@wlabel.co>
2021-03-28 22:25:16 -0400

    add comments, conversion to tensor

--- main.py
+++ main.py
@@ -1,24 +1,60 @@
+# conda install tensorflow
 import tensorflow as tf
 import os
+# conda install pillow
+from PIL import Image
+# conda install torchvision
+from torchvision.transforms import ToTensor
 
 from tensorflow.keras import datasets, layers, models
 #import matplotlib.pyplot as plt
+
+# place your dataset path here
+# dataset download links:
+# https://web.inf.ufpr.br/vri/databases/ufpr-alpr/
+# https://www.kaggle.com/alessiocorrado99/animals10
+# https://www.kaggle.com/prasunroy/natural-images
 datasetPath = "/Users/michael/documents/dev/licensespot/dataset"
 
 class CNNData:
-    def __init__(datasetPath):
-        self.licensePlates = [];
-        self.randomData = [];
+    def __init__(self, datasetPath):
+        self.licensePlates = []
+        self.randomData = []
 
-        for dirpath, dirnames, filenames in os.walk(dataset):
+        # crawl dataset path
+        for dirpath, dirnames, filenames in os.walk(datasetPath):
             #whatever you want to do with these folders
+            # go through each filename
             for filename in filenames:
-                if "licenseplates" in dirpath:
-                    licensePlates.append(filename);
-                else:
-                    randomData.append(filename);
+                # make sure its an image
+                if (".jpg" in filename) or (".png" in filename) or (".jpeg" in filename):
+                    # if from licenseplate dataset, place it in the licensePlate Set
+                    # else put it in the Random set
+                    if "licenseplates" in dirpath:
+                        self.licensePlates.append(CNNImage(dirpath, filename))
+                    else:
+                        self.randomData.append(CNNImage(dirpath, filename))
+
+
+class CNNImage:
+    def __init__(self, dirpath, filename):
+        self.filepath = dirpath + '/' + filename
+        self.filename = filename
+        self.isPlate = 0 # not a license plate
+        if "licenseplates" in dirpath:
+            self.isPlate = 1 # is a license plate
+
+        print(self.filepath)
+        # convert to tensor and output
+        self.image = Image.open(self.filepath)
+        self.image = ToTensor()(self.image).unsqueeze(0) # unsqueeze to add artificial first dimension
+        print(self.image)
+
+
 
 
 if __name__ == "__main__":
     print('Hello World')
+
+test = CNNData(datasetPath)
 
